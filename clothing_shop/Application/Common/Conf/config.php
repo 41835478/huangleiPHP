fb0564758fa7622afb22905ebc764d587a3fec45
黄磊 <huanglei@higohappy.com>
2017-11-06 17:28:46 +0800

    hello

--- clothing_shop/Application/Common/Conf/config.php
+++ clothing_shop/Application/Common/Conf/config.php
@@ -3,10 +3,10 @@ return array(
 
 
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'sqld.duapp.com:4050',
-    'DB_USER' => '3e1b7b9a6daf4c5babceac749c322b51',
-    'DB_PWD' => '87d43e0bbe2441cc9a2b207d3c475b99',
-    'DB_NAME' => 'GUhndhAdAnCEMARJOdxz',
+    'DB_HOST' => '127.0.0.1:3306',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_NAME' => 'clothing',
     'DB_PREFIX' =>'sh_',
 
 );
